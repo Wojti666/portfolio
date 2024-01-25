589c0ebf5cf8a81fe3fe08e635abad48ce0fd3c1
Wojti <wojcieszko666@o2.pl>
2024-01-25 20:36:38 +0100

    contact section almost complette

--- src/js/app.js
+++ src/js/app.js
@@ -77,5 +77,25 @@ timeline
 		'3'
 	)
 
-	//====================================================================================================
-	
+//====================================================================================================
+const msgStatus = document.querySelector('.msg-status')
+
+console.log(document.location.search)
+
+if (document.location.search === '?mail_status=sent') {
+	msgStatus.classList.add('success')
+	msgStatus.textContent = 'wiadomość wysłana'
+
+	setTimeout(() => {
+		msgStatus.classList.remove('success')
+	}, 3000)
+}
+if (document.location.search === '?mail_status=error') {
+	msgStatus.classList.add('error')
+	msgStatus.textContent = 'wystąpił błąd, spróbuj ponownie.'
+
+	setTimeout(() => {
+		msgStatus.classList.remove('error')
+	}, 3000)
+}
+//========================================================================================================================================
